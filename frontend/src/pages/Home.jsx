04cdd65a2f61e3cefd0b3467cb7ede172e2d75c3
rishitkamboj <rishit242@gmail.com>
2024-06-24 16:24:56 +0530

    made some changes to dashboard

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,42 @@
-import Hero from "../components/Hero";
-import NavBar from "../components/NavBar";
+import React, { useEffect } from 'react';
+import { useNavigate} from 'react-router-dom'; 
+import Hero from '../components/Hero';
 
 
-function Home(){
-     return(<div>
-        <Hero />
-     </div>)
+function Home() {
+  const router= useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token'); 
+
+    if (token) {
+      fetch('https://example.com/api/validateToken', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+          router('/dashboard');
+          } else {
+         
+            console.error('Token validation failed');
+          }
+        })
+        .catch((error) => {
+          console.error('Error validating token:', error);
+        });
+    }
+  }, []);
+
+  return (
+    <div>
+   
+      <Hero />
+    </div>
+  );
 }
-
 
 export default Home;
