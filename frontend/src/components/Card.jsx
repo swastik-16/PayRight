ba6080214dce574d332801fc8ed2db627ce8da91
rishitkamboj <rishit242@gmail.com>
2024-06-25 17:53:56 +0530

    dashboard

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -45,7 +45,7 @@ const CardComponent = ({ name, to }) => {
           navigate("/dashboard");
     } catch (error) {
       console.error("Error sending money:", error);
-      alert("Failed to send money. Please try again. "+error.response.data.message); 
+      alert("Failed to send money "+error.response.data.message); 
     }
   };
 
