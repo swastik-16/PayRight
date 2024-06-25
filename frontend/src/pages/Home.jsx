928de6ba1bb1d49dabd563748e7c625a4e0ca0a6
rishitkamboj <rishit242@gmail.com>
2024-06-25 17:57:24 +0530

    ughhh

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
-      navigate("/dashboard");
+      router("/dashboard");
     }
   }, [navigate]);
 
